4668f462ebc5dfdc0ac86dbeb1fa1f3ea6ddb3f0
Fernando <fernandoasfj@gmail.com>
2019-01-14 14:22:31 -0300

    Add some colors =)

--- workshop-docker.sh
+++ workshop-docker.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
 
+function in_color {
+  tput -Txterm setaf ${1}; echo -e ${2}; tput -Txterm sgr0;
+}
+
+function error { 
+  in_color 1 "${1}"
+}
+function warning { 
+  in_color 3 "${1}" 
+}
+function notice { 
+  in_color 6 "${1}"
+}
+
 function check_presence {
   [[ -n `which $1` ]]
 }
@@ -15,30 +29,30 @@ function image_downloaded {
 
 function ensure_image_is_present {
   if ! image_downloaded $1 ; then
-    echo "Pulling image $1"
+    notice "Pulling image $1"
     pull_image $1
   else
-    echo "Found image $1!"
+    notice "Found image $1!"
   fi
 }
 
 function ensure_brew_is_installed {
   if ! check_presence 'brew' ; then
-    echo 'Ops! We need brew installed =('
-    echo 'Please execute the following command and try again:'
-    echo '  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+    error 'Ops! We need brew installed =('
+    error 'Please execute the following command and try again:'
+    error '  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
     exit 1
   fi
 }
 
 function install {
-  echo "Installing $1"
+  notice "Installing $1"
   `brew cask install $1`
 }
 
 function install_if_not_present {
   if check_presence $1 ; then
-    echo "Found $1!"
+    notice "Found $1!"
   else
     install $1
   fi
@@ -51,8 +65,8 @@ function setup_docker {
 
 function ensure_docker_is_running {
   if ! docker info &> /dev/null ; then
-    echo 'It looks Docker is not started.'
-    echo 'Please start the server and run this script again.'
+    error 'It looks Docker is not started.'
+    error 'Please start the server and run this script again.'
     exit 1
   fi
 }
@@ -64,15 +78,15 @@ function pull_images {
 
 function redis_notice {
   if check_presence 'redis-cli' ; then
-    echo 'It looks like you have redis installed.'
-    echo 'Please make sure the server is not running before the workshop.'
+    warning 'It looks like you have redis installed.'
+    warning 'Please make sure the server is not running before the workshop.'
   fi
 }
 
 function nodejs_notice {
   if ! check_presence 'node' ; then
-    echo 'It looks like dont have node installed.'
-    echo 'Please install it before the workshop. It can be done via: brew install node'
+    error 'It looks like dont have node installed.'
+    error 'Please install it before the workshop. It can be done via: brew install node'
     exit 1
   fi
 }
@@ -82,4 +96,4 @@ pull_images
 redis_notice
 nodejs_notice
 
-echo 'All set!'
+notice 'All set!'
